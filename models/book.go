23f26e4e050a66c7254d9c38853c07e94600ab55
Pramudya Pratama <43670602+pramudyapratama203@users.noreply.github.com>
2025-07-26 20:03:33 +0700

    Update book.go

--- models/book.go
+++ models/book.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Book struct {
-	ID primitive.ObjectID `bson:"_id" json:"id,omitempty"`
+	ID primitive.ObjectID `bson:"_id" json:"id,omitempty"` // menggunakkan _id
 	Title string `bson:"title" json:"title"`
 	Author string `bson:"author" json:"author"`
 	ISBN string `bson:"isbn" json:"isbn"`
